85438879b5a83e2e05c28952db81047f67b99a56
david <davidcol123dm@gmail.com>
2024-10-30 23:30:23 -0500

    mas metodos mejorados

--- src/services/MascotasServices.js
+++ src/services/MascotasServices.js
@@ -1,25 +1,26 @@
-
-import  axios  from "axios";
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5092/api'
-})
+  baseURL: 'http://localhost:5092/api',
+  headers: {
+    'Content-Type': 'application/json' 
+  }
+});
 
 export default {
   getDataMascotas() {
-    return api.get(`/mascotas`)
+    return api.get(`/mascotas`);
   },
 
   postDataMascotas(data) {
-    api.post(`/mascotas`, data)
+    return api.post(`/mascotas`, data); 
   },
 
   putDataMascotas(data, id) {
-    api.put(`/mascotas/${id}`, data)
+    return api.put(`/mascotas/${id}`, data); 
   },
 
-  deleteDataMascotas(data, id) {
-    api.delete(`/mascotas/${id}`, data)
-  },
-  
-}
+  deleteDataMascotas(id) {
+    return api.delete(`/mascotas/${id}`);
+  }
+};
