ec92cc619c8c40397f479162cb3b929b3cfe7441
david <davidcol123dm@gmail.com>
2024-10-30 22:09:06 -0500

    ahora coincide el id

--- src/components/AppMascota.vue
+++ src/components/AppMascota.vue
@@ -67,11 +67,11 @@
     <div class="grid md:grid-cols-2  gap-5 mt-10 ">
       <button 
         type="button"
-        @click="$emit('obtener-mascota', mascota.id)"
+        @click="$emit('obtener-mascota', mascota.mas_Id)"
         class="block w-full py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg">Editar</button>
 
       <button type="button"
-        @click="$emit('eliminar-mascota', mascota.id)"
+        @click="$emit('eliminar-mascota', mascota.mas_Id)"
         class="block w-full py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg">Eliminar</button>
     </div>
   </div>
